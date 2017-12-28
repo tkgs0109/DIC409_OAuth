dca25c51743866f2d0366d45628e2a1e32a75bdf
tkgs0109 <subtkgs@gmail.com>
2017-12-28 19:31:09 +0900

    routing changed, index.html.erb edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'oauth_test/index'
+  root :to => 'oauth_test#index'
 
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-
 end
